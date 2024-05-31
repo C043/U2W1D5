f49327e0c88179b960b05b0f6f5f5e5c9c69872c
C043 <mariofragnitoph@gmail.com>
2024-05-31 12:49:44 +0200

    Adds navbar animation

---
+++ assets/JavaScript/script.js
@@ -0,0 +1,14 @@
+window.onscroll = (event) => {
+  console.log(event);
+  const nav = document.querySelector("nav");
+  const getStarted = document.querySelector("nav a:last-of-type");
+  if (window.scrollY > 300) {
+    nav.classList.add("changed-nav");
+    getStarted.classList.remove("black-button");
+    getStarted.classList.add("changed-button");
+  } else {
+    nav.classList.remove("changed-nav");
+    getStarted.classList.remove("changed-button");
+    getStarted.classList.add("black-button");
+  }
+};
