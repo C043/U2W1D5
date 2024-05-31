4d6315cee56cccc2b86320edcc31fb4c640a7548
C043 <mariofragnitoph@gmail.com>
2024-05-31 12:58:21 +0200

    Fixed trending title

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -2,7 +2,7 @@ window.onscroll = (event) => {
   console.log(event);
   const nav = document.querySelector("nav");
   const getStarted = document.querySelector("nav a:last-of-type");
-  if (window.scrollY > 300) {
+  if (window.scrollY > 400) {
     nav.classList.add("changed-nav");
     getStarted.classList.remove("black-button");
     getStarted.classList.add("changed-button");
