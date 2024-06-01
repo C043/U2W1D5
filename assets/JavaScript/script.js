6d290e5f0fe8a77d31922ab3ff1c38ac835c92ee
Mario Fragnito <mariofragnitoph@gmail.com>
2024-06-01 11:41:17 +0200

    Optimized mAnimation

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,15 +1,24 @@
+// Filtro le m in modo da prendere solo quelle che mi servono
+
+const mElements = document.getElementsByTagName("g");
+const mElementsFiltered = Array.from(mElements).filter(
+  (element) => element.getAttribute("opacity") != undefined
+);
+mElementsFiltered.shift();
+
+// Funzione che anima le m
 const mAnimation = setInterval(() => {
-  const mElements = document.getElementsByTagName("g");
-  const randomNum = Math.floor(Math.random() * mElements.length) + 2;
-  const currentElement = mElements[randomNum];
-  const currentOpacity = currentElement.getAttributeNS(null, "opacity");
+  const randomNum = Math.floor(Math.random() * mElementsFiltered.length);
+  const currentElement = mElementsFiltered[randomNum];
+  const currentOpacity = currentElement.getAttribute("opacity");
   if (currentOpacity === "0") {
-    currentElement.setAttributeNS(null, "opacity", 1);
+    currentElement.setAttribute("opacity", 1);
   } else if (currentOpacity === "1") {
-    currentElement.setAttributeNS(null, "opacity", 0);
+    currentElement.setAttribute("opacity", 0);
   }
 }, 10);
 
+// Funzione che anima la navbar
 window.onscroll = () => {
   const nav = document.querySelector("nav");
   const getStarted = document.querySelector("nav a:last-of-type");
